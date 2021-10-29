c999152764b9400336f4c43760c0d47c45982fe6
Sherwin <sherwingaddis@gmail.com>
2021-10-29 06:43:19 -0400

    update module code

--- interface/modules/custom_modules/oe-module-batching-creditcard/settings.php
+++ interface/modules/custom_modules/oe-module-batching-creditcard/settings.php
@@ -12,3 +12,32 @@
  */
 
 echo "Settings Go Here on this page";
+
+use OpenEMR\Core\Header;
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Batch Credit Card Settings</title>
+    <?php Header::setupHeader() ?>
+</head>
+<body>
+    <div class="container">
+        <div>
+            <form action="settings.php" method="post">
+                <label for="user">Username</label>
+                <input type="text" name="user" id="user" value="<?php  ?>" placeholder="Paytrace Username">
+                <label for="password">Password</label>
+                <input type="text" name="password" id="password" value="<?php ?>" placeholder="Password">
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+    </div><!-- end of container div -->
+</body>
+</html>
