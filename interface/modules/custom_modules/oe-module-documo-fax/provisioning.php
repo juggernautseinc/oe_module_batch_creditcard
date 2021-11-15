3d016b133936503b0355d1291bd43e5cc79596d6
Sherwin <sherwingaddis@gmail.com>
2021-11-14 20:52:07 -0500

    Adding styling to the buttons

--- interface/modules/custom_modules/oe-module-documo-fax/provisioning.php
+++ interface/modules/custom_modules/oe-module-documo-fax/provisioning.php
@@ -30,7 +30,52 @@ require_once dirname(__FILE__, 4) . "/globals.php";
         #transfer {
             display: none;
         }
-
+        . provision {
+            box-shadow: 0px 10px 14px -7px #276873;
+            background:linear-gradient(to bottom, #599bb3 5%, #408c99 100%);
+            background-color:#599bb3;
+            border-radius:8px;
+            display:inline-block;
+            cursor:pointer;
+            color:#ffffff;
+            font-family:Arial;
+            font-size:20px;
+            font-weight:bold;
+            padding:13px 32px;
+            text-decoration:none;
+            text-shadow:0px 1px 0px #3d768a;
+        }
+        . provision:hover {
+            background:linear-gradient(to bottom, #408c99 5%, #599bb3 100%);
+            background-color:#408c99;
+        }
+        . provision:active {
+            position:relative;
+            top:1px;
+        }
+        . port {
+            box-shadow: 0px 10px 14px -7px #fff6af;
+            background:linear-gradient(to bottom, #ffec64 5%, #ffab23 100%);
+            background-color:#ffec64;
+            border-radius:8px;
+            display:inline-block;
+            cursor:pointer;
+            color:#333333;
+            font-family:Arial;
+            font-size:20px;
+            font-weight:bold;
+            padding:13px 32px;
+            text-decoration:none;
+            text-shadow:0px 1px 0px #ffee66;
+        }
+        . port:hover {
+            background:linear-gradient(to bottom, #ffab23 5%, #ffec64 100%);
+            background-color:#ffab23;
+        }
+        . port:active {
+            position:relative;
+            top:1px;
+        }
     </style>
     <script>
         function toggleOrder(id) {
@@ -51,8 +96,8 @@ require_once dirname(__FILE__, 4) . "/globals.php";
 <body>
     <div class="container-fluid">
         <div class="buttons">
-            <button id="provision" onclick="toggleOrder('order')">Provision New Number</button>
-            <button id="porting" onclick="toggleTransfer('transfer')">Port Current Number</button>
+            <button id="provision" class="provision" onclick="toggleOrder('order')">Provision New Number</button>
+            <button id="porting" class="port" onclick="toggleTransfer('transfer')">Port Current Number</button>
         </div>
         <div id="order">
             <p>Order a new number</p>
