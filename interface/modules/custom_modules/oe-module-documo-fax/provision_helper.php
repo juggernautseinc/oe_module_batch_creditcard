3f95918ad46db08ef246da7b130c65545e4906fa
Sherwin <sherwingaddis@gmail.com>
2021-11-19 14:51:26 -0500

    building the classes to retrieve the available numbers - show errors

--- interface/modules/custom_modules/oe-module-documo-fax/provision_helper.php
+++ interface/modules/custom_modules/oe-module-documo-fax/provision_helper.php
@@ -9,6 +9,10 @@
  *
  */
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 use OpenEMR\Module\Documo\Provisioning;
 use OpenEMR\Common\Csrf\CsrfUtils;
 
