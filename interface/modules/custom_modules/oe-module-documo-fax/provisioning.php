216f8e8ca8ae06c16a94f48239b08b9d89f34619
Sherwin <sherwingaddis@gmail.com>
2021-11-21 07:00:54 -0500

    building the classes to retrieve the available numbers - some more css and variables

--- interface/modules/custom_modules/oe-module-documo-fax/provisioning.php
+++ interface/modules/custom_modules/oe-module-documo-fax/provisioning.php
@@ -107,7 +107,7 @@ require_once dirname(__FILE__, 4) . "/globals.php";
             <button id="provision" class="provision mr-4" onclick="toggleOrder('order')"><?php echo xlt("Provision New Number") ?></button>
             <button id="porting" class="port " onclick="toggleTransfer('transfer')"><?php echo xlt("Port Current Number") ?></button>
         </div>
-        <div class="row">
+        <div class="row mb-4 card">
             <div class="form-group col-5 mr-4">
                 <label for="provision-type"><?php echo xlt("Provision Type") ?></label>
                 <div role="combobox" class="form-control" aria-expanded="false">
@@ -117,7 +117,7 @@ require_once dirname(__FILE__, 4) . "/globals.php";
                 </div>
             </div>
             <div class="form-group col-6">
-                <lable for="search-type"><?php echo xlt("Search Type") ?></lable>
+                <label for="search-type"><?php echo xlt("Search Type") ?></label>
                 <div role="combobox" class="form-control" aria-expanded="false">
                     <select class="form-control" id="searchtype" name="searchtype">
                         <option><?php echo xlt("Select") ?></option>
