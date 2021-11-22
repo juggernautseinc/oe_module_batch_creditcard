d199aee6578d05e9133bba786a5d6f8e75a29702
Sherwin <sherwingaddis@gmail.com>
2021-11-21 23:34:39 -0500

    building the classes to retrieve the available numbers - debugging

--- interface/modules/custom_modules/oe-module-documo-fax/js/ajax.js
+++ interface/modules/custom_modules/oe-module-documo-fax/js/ajax.js
@@ -7,7 +7,7 @@ function numberType() {
     }
 }
 document.getElementById('checkfornumbers').addEventListener("click", numberSearch);
-function numberSearch(e) {
+async function numberSearch(e) {
     e.preventDefault();
     const prefix = document.getElementById('prefix').value;
     const city = document.getElementById('city').value;
@@ -16,7 +16,7 @@ function numberSearch(e) {
     const token = document.getElementById('token').value;
     let output = '<h3>Available Numbers</h3>';
 
-    fetch('provision_helper.php', {
+    await fetch('provision_helper.php', {
         method: 'POST',
         headers: {
             'Accept': 'application/json, text/plain, */*',
@@ -24,25 +24,26 @@ function numberSearch(e) {
             'mode': 'cors',
             'credentials': 'same-origin'
         },
-        body: JSON.stringify({type:type, areacode:prefix, city:city, zip:zip})
+        body: JSON.stringify({type: type, areacode: prefix, city: city, zip: zip})
     })
         .then((res) => res.json())
         .then((data) => {
             if (data != '') {
                 data = JSON.parse(data);
                 console.log(data);
+                console.log(prefix);
                 //data.forEach(function (rows) {
-                  //  output += `
-           //<div>
-             //   <p>${rows.number}</p>
-            //</div>
-            //`;
+                //  output += `
+                //<div>
+                //   <p>${rows.number}</p>
+                //</div>
+                //`;
                 //});
                 document.getElementById('numberdisplay').innerHTML = output;
             } else {
                 console.log("Data Empty");
             }
-        });//.catch((err) => console.log(err));
+        }).catch((err) => console.log(err));
 
 
 }
