bf3e9b665dbcf10c99d319bf1b6e0a1c7b7384a5
Sherwin <sherwingaddis@gmail.com>
2021-11-21 23:15:27 -0500

    building the classes to retrieve the available numbers - debugging

--- interface/modules/custom_modules/oe-module-documo-fax/js/ajax.js
+++ interface/modules/custom_modules/oe-module-documo-fax/js/ajax.js
@@ -24,7 +24,7 @@ function numberSearch(e) {
             'mode': 'cors',
             'credentials': 'same-origin'
         },
-        body:JSON.stringify({type:type, areacode:prefix, city:city, zip:zip})
+        body: JSON.stringify({type:type, areacode:prefix, city:city, zip:zip})
     })
         .then((res) => res.json())
         .then((data) => {
