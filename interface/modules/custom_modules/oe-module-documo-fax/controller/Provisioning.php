c6bea4cb44e02d510e7671aa0d4b0edb6b4fd1e7
Sherwin <sherwingaddis@gmail.com>
2021-11-22 15:06:41 -0500

    building the classes to retrieve the available numbers - debugging return

--- interface/modules/custom_modules/oe-module-documo-fax/controller/Provisioning.php
+++ interface/modules/custom_modules/oe-module-documo-fax/controller/Provisioning.php
@@ -31,7 +31,7 @@ class Provisioning
     {
         if (isset($this->areaCode)) {
             //return $this->dispatch->findAvailableFaxNumber($this->type, $this->areaCode, $this->city, $this->zipcode);
-            return $this->dispatch->findAvailableFaxNumber($this->areaCode);
+            return $this->dispatch->findAvailableFaxNumber(757);
         } else {
             return 'error';
         }
