5f9bfcd76c769d19b2e2154b3d583fcb807395ca
Sherwin <sherwingaddis@gmail.com>
2021-11-23 05:26:12 -0500

    building the classes to retrieve the available numbers - display styling

--- interface/modules/custom_modules/oe-module-documo-fax/js/ajax.js
+++ interface/modules/custom_modules/oe-module-documo-fax/js/ajax.js
@@ -26,8 +26,8 @@ async function numberSearch(e) {
             console.log(response);
             response.rows.forEach(function(row) {
                 output += `
-                <div class="card col-6 mb-4 checkbox ">
-                    <input type="checkbox" class="styled" name="${row.number_e164}" value="${row.number_e164}">
+                <div class="card col-12 mb-4 checkbox ">
+                    <input id="${row.number_e164}" type="checkbox" class="styled" name="${row.number_e164}" value="${row.number_e164}">
                     <label for="${row.number_e164}">${row.number_e164}, ${row.regional_data.rate_center}, ${row.regional_data.state}, ${row.regional_data.country_iso}</label>
                 </div>
                 `;
