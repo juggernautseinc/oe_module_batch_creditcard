044c3721533c883be961c4e7f6fd34ee18740881
Sherwin <sherwingaddis@gmail.com>
2021-12-08 08:45:13 -0500

    Refactoring twig

--- interface/modules/custom_modules/oe-module-documo-fax/fax/faxque.php
+++ interface/modules/custom_modules/oe-module-documo-fax/fax/faxque.php
@@ -9,4 +9,28 @@
  *
  */
 
+use OpenEMR\Core\Header;
+use OpenEMR\Common\Twig\TwigContainer;
+use OpenEMR\Modules\Documo\ApiDispatcher;
+use OpenEMR\Modules\Documo\Database;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
+require_once dirname(__FILE__, 5) . "/globals.php";
+
+$header = Header::setupHeader(['common']);
+$path = dirname(__FILE__, 2) . "/templates";
+$twigloader = new TwigContainer($path, $GLOBALS['kernel']);
+$status = new ApiDispatcher();
+$twig = $twigloader->getTwig();
+$twig->addExtension(new Twig_Extension_Debug());
+
+try {
+    print $twig->render('que.twig', [
+        'pageTitle' => 'Fax Que',
+        'header' => $header,
+    ]);
+} catch (LoaderError | RuntimeError | SyntaxError $e) {
+    print $e->getMessage();
+}
