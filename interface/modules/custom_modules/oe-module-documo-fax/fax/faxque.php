72ea888eb39723ad549a96f4a340766199c5d413
Sherwin <sherwingaddis@gmail.com>
2021-12-09 13:20:59 -0500

    Implementing fax cue twig - troublshooting code trap 3

--- interface/modules/custom_modules/oe-module-documo-fax/fax/faxque.php
+++ interface/modules/custom_modules/oe-module-documo-fax/fax/faxque.php
@@ -21,7 +21,7 @@ require_once dirname(__FILE__, 5) . "/globals.php";
 
 $path = dirname(__FILE__, 2) . "/templates";
 $twigloader = new TwigContainer($path, $GLOBALS['kernel']);die('here loader');
-$status = new ApiDispatcher();
+$status = new ApiDispatcher();die('here loader dispatch');
 $twig = $twigloader->getTwig();
 $twig->addExtension(new Twig_Extension_Debug());
 
