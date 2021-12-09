ff7902f7683fe571ee105cd253e1c89c1d7baf43
Sherwin <sherwingaddis@gmail.com>
2021-12-09 15:34:59 -0500

    Implementing fax cue twig -

--- interface/modules/custom_modules/oe-module-documo-fax/fax/faxque.php
+++ interface/modules/custom_modules/oe-module-documo-fax/fax/faxque.php
@@ -11,7 +11,7 @@
 
 use OpenEMR\Common\Twig\TwigContainer;
 
-use OpenEMR\Documo\ApiDispatcher;
+use OpenEMR\Modules\Documo\ApiDispatcher;
 use OpenEMR\Modules\Documo\Database;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
