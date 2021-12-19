6293c54668d0dea05f9457ef071eb1f3b83725e7
Sherwin <sherwingaddis@gmail.com>
2021-12-19 17:45:25 -0500

    Added webhook folder - start with accountId - store webhook

--- interface/modules/custom_modules/oe-module-documo-fax/fax/faxque.php
+++ interface/modules/custom_modules/oe-module-documo-fax/fax/faxque.php
@@ -49,7 +49,9 @@ if (!$_POST['number']) {
     $hook = new Database();
     $number_uuid = $hook->getFaxNumbers();
     $user = $hook->getUserInfo();
-    var_dump($user);
+    $facility = $hook->getAllAccountInfo();
+
+    var_dump($facility);
     //this url will change based on where I put the call to the hook function
     $hookurl = substr(getWebHookURI(), 0, -10);
     $hookstring = str_replace(PHP_EOL, '', $hookstring); //remove returns
