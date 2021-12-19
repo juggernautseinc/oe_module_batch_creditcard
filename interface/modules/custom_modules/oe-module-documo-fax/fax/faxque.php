58e8763aa6e4194f6069ba5c53bb4fae7fdbaa94
Sherwin <sherwingaddis@gmail.com>
2021-12-19 16:43:42 -0500

    Added webhook folder - start with accountId and numberId testing uuid

--- interface/modules/custom_modules/oe-module-documo-fax/fax/faxque.php
+++ interface/modules/custom_modules/oe-module-documo-fax/fax/faxque.php
@@ -48,7 +48,7 @@ if (!$_POST['number']) {
     }
     $hook = new Database();
     $number_uuid = $hook->getFaxNumbers();
-    var_dump($number_uuid);
+    var_dump($number_uuid[0]['uuid']);
     //this url will change based on where I put the call to the hook function
     $hookurl = substr(getWebHookURI(), 0, -10);
     $hookstring = str_replace(PHP_EOL, '', $hookstring); //remove returns
@@ -63,7 +63,7 @@ if (!$_POST['number']) {
         &events=%7B%20%22fax.inbound%22%3A%20true%2C%20%22fax.outbound%22%3A%20true%2C%20%22fax.outbound.extended%22%3A%20true%2C%20%22user.create%22%3A%20true%2C%20%22user.delete%22%3A%20true%2C%20%22number.add%22%3A%20false%2C%20%22number.release%22%3A%20false%2C%20%22document.complete%22%3A%20false%2C%20%22document.failed%22%3A%20false%20%7D
         &auth=example
         &accountId=' . $hook->getAccountId() . '
-        &numberId=d1077489-5ea1-4db1-9760-853f175e8288
+        &numberId=' . $number_uuid[0]['uuid'] . '
         &attachmentEnabled=false
         &notificationEmails=example%2Cexample';
         var_dump($hookstring);
