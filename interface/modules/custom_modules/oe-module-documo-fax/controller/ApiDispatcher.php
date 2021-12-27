c1b8209d2162c83e3e1243f24c892522623cc90b
Sherwin <sherwingaddis@gmail.com>
2021-12-26 21:21:20 -0500

    Post Array causing issue

--- interface/modules/custom_modules/oe-module-documo-fax/controller/ApiDispatcher.php
+++ interface/modules/custom_modules/oe-module-documo-fax/controller/ApiDispatcher.php
@@ -193,13 +193,11 @@ class ApiDispatcher
     {
         $curl = curl_init();
         $postData = array("faxNumber"=>$this->faxNumber,"'attachments'"=>$this->filePost,"coverPage"=>'false',"coverPageId"=>'','tags'=>'','recipientName'=>$this->name,'senderName'=>$this->senderName,'subject'=>$this->subject,'callerId'=>$this->callerID,'notes'=>'','cf'=>'','scheduledDate'=>$this->schedule,'webhookId'=>'');
-        $postData = str_replace(PHP_EOL, '', $postData);
-        $postData = str_replace(' ', '', $postData);
         file_put_contents("/var/www/html/errors/postData.txt", print_r($postData,true));
         curl_setopt_array($curl, array(
             CURLOPT_URL => 'https://api.documo.com/v1/faxes',
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
+            CURLOPT_ENCODING => 1,
             CURLOPT_MAXREDIRS => 10,
             CURLOPT_TIMEOUT => 0,
             CURLOPT_FOLLOWLOCATION => true,
@@ -217,7 +215,6 @@ class ApiDispatcher
         } else {
             return $status;
         }
-
     }
 
     public function setWebHook($hookstring) : string
