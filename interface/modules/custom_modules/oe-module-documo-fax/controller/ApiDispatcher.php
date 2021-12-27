2800c61fecdc1605a38539f5673cc16f1e3c160f
Sherwin <sherwingaddis@gmail.com>
2021-12-27 11:40:00 -0500

    Post Array causing issue - content type correction

--- interface/modules/custom_modules/oe-module-documo-fax/controller/ApiDispatcher.php
+++ interface/modules/custom_modules/oe-module-documo-fax/controller/ApiDispatcher.php
@@ -192,7 +192,7 @@ class ApiDispatcher
     public function sendFax()
     {
         $curl = curl_init();
-        $postData = array("faxNumber"=>$this->faxNumber,"'attachments'"=>$this->filePost,"coverPage"=>'false',"coverPageId"=>'','tags'=>'','recipientName'=>$this->name,'senderName'=>$this->senderName,'subject'=>$this->subject,'callerId'=>$this->callerID,'notes'=>'','cf'=>'','scheduledDate'=>$this->schedule,'webhookId'=>'');
+        $postData = array("faxNumber"=>$this->faxNumber,"'attachments'"=>$this->filePost,"coverPage"=>'false',"coverPageId"=>'','tags'=>'','recipientName'=>$this->name,'senderName'=>$this->senderName,'subject'=>$this->subject,'callerId'=>$this->callerID,'notes'=>'','scheduledDate'=>$this->schedule);
         file_put_contents("/var/www/html/errors/postData.txt", print_r($postData,true));
         curl_setopt_array($curl, array(
             CURLOPT_URL => 'https://api.documo.com/v1/faxes',
@@ -216,7 +216,8 @@ class ApiDispatcher
         if ($status === 200) {
             return $response;
         } else {
-            return $status;
+            $error = "Please verify the fax number before trying again " . $status;
+            return $error;
         }
     }
 
