0e76b5e21776dcd6025ffb02f77c0cac03f61633
Sherwin <sherwingaddis@gmail.com>
2022-01-01 09:38:44 -0500

    Building history display

--- interface/modules/custom_modules/oe-module-documo-fax/fax/faxque.php
+++ interface/modules/custom_modules/oe-module-documo-fax/fax/faxque.php
@@ -7,6 +7,7 @@
  *  copyright Copyright (c )2021. Sherwin Gaddis <sherwingaddis@gmail.com>
  *  license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  *
+ *  This needs to be move into a set of classes to handle these process and break up this cluster !!@#$%
  */
 
 use OpenEMR\Common\Twig\TwigContainer;
