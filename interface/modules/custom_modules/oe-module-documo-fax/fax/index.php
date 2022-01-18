44c220f3292965a962dc4c2c8db6cd244e04ee04
Sherwin <sherwingaddis@gmail.com>
2022-01-18 17:45:10 -0500

    finalizing webhook - moving file to inbox TS

--- interface/modules/custom_modules/oe-module-documo-fax/fax/index.php
+++ interface/modules/custom_modules/oe-module-documo-fax/fax/index.php
@@ -27,7 +27,7 @@ $uri = explode('/', $uri);
 
 if ($uri[7] == 'inbound') {
     $inboundFaxFilesize = $_FILES['attachment']['size'];
-    $inbound = dirname(__FILE__, 6) . "/sites/default/documents/documo/inbound/";
+    $inbound = "../../../../../sites/default/documents/documo/inbound/";
     move_uploaded_file($_FILES['attachment']['tmp_name'], $inbound . $_FILES['attachment']['name']);
     http_response_code(200);
 }
