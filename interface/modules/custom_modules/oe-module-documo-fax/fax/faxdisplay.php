d929e9d23cb21ccc91e3db3f8b4bcdf2fcfe9e74
Sherwin <sherwingaddis@gmail.com>
2022-02-05 07:11:44 -0500

    Inbound build continued - display

---
+++ interface/modules/custom_modules/oe-module-documo-fax/fax/faxdisplay.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * package   OpenEMR
+ *  link      http://www.open-emr.org
+ *  author    Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  copyright Copyright (c )2022. Sherwin Gaddis <sherwingaddis@gmail.com>
+ *  license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ *
+ */
+
+require_once dirname(__FILE__, 5) . "/globals.php";
+require_once dirname(__FILE__, 2) . "/vendor/autoload.php";
+
+$file = $_GET['filename'];
+$location = $GLOBALS['webroot'] . "/sites/" . $_SESSION['site_id'] . "/documents/documo/inbound/";
+
+$display = file_get_contents($location . $file);
+
